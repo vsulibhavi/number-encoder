13b7b36c1c16ee23a6d1c2e75818a0a710db337e
Vishal Sulibhavi <vsulibhavi@gmail.com>
2014-05-19 00:33:26 +0530

    added the test data and input enhancement to algorithm

--- src/main/java/org/encoder/setup/InputGatherer.java
+++ src/main/java/org/encoder/setup/InputGatherer.java
@@ -1,12 +1,17 @@
 package org.encoder.setup;
 
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.HashMap;
+import java.util.List;
+
+import org.myTrie.Trie;
 
 public class InputGatherer {
 
 	private HashMap<Character, Character> mapOfAlphaToNum;
-	
+	private Trie trie = new Trie();
+	private HashMap<String, List<String>> mapOfNumToDictWords = new HashMap<String, List<String>>();
 	private ReadDictionery readDictionery = new ReadDictionery();
 	
 	public InputGatherer() {
@@ -28,6 +33,42 @@ public class InputGatherer {
 	
 	public void initializeNumberToDictWordsLookup()
 	{
+		StringBuffer tempBuffer  = new StringBuffer();
+		
+		for(String word : readDictionery.getDictioneryWords())
+		{
+			
+			for(Character character : word.toCharArray())
+			{
+				
+				tempBuffer.append(mapOfAlphaToNum.get(character));
+				
+			}
+			
+			String num = tempBuffer.toString();
+			if(mapOfNumToDictWords.get(num) == null)
+			{
+				List<String> words = new ArrayList<String>();
+				words.add(word);
+				mapOfNumToDictWords.put(num, words );
+			}
+			else
+			{
+				List<String> words = mapOfNumToDictWords.get(num);
+				words.add(word);
+				mapOfNumToDictWords.put(num, words );
+			}
+		}
+		
+		addToTrie();
+	}
+
+	private void addToTrie() {
+
+		for(String num : mapOfNumToDictWords.keySet())
+		{
+			trie.put(num, mapOfNumToDictWords.get(num));
+		}
 		
 	}
 }
